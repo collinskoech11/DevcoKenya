73d85e98f73c1fdaaff5ee631f577d1b08b6f9b0
collinskoech11 <collinskoechck34@gmail.com>
2020-08-05 16:01:33 +0300

    update

--- js/script.js
+++ js/script.js
@@ -26,29 +26,3 @@ window.addEventListener('scroll',
 		const topcontent = document.querySelector('.topcontent');
 		topcontent.classList.toggle("transition",window.scrollY > 30)
 	})
-$(document).ready(function(){
-var refreshId;
-var restartAnimation = function() {
-    clearInterval(refreshId);
-    refreshId = setInterval( function() 
-    {    
-        $('.box').each(function() {
-            if ($(this).offset().left < 0) {
-                $(this).css("left", "150%");
-            } else if ($(this).offset().left > $('#container').width()) {
-                $(this).animate({
-                    left: '50%'
-                }, 500 );
-            } else {
-                $(this).animate({
-                    left: '-150%'
-                }, 500 );
-            }
-        });
-    },1000);
-}
-
-restartAnimation()
-
-
-});
